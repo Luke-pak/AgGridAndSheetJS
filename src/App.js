dc9b0c710d5b1e76880d01494a6cf8b4283bccdc
Luke-pak <icbm07@hanmail.net>
2021-05-31 17:30:21 +0900

    001

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
+import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import XLSX from 'xlsx';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App() {
+  const [rowData, setRowData] = useState([]);
+  const gridRef = useRef(null);
+
+  const FileImport = e => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(e.target.files[0]);
+
+    reader.onload = function () {
+      const fileData = reader.result;
+      const wb = XLSX.read(fileData, { type: 'binary' });
+      wb.SheetNames.forEach(function (sheetName) {
+        var rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+        setRowData(rowObj);
+      });
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ width: '100vw', height: 800 }}>
+      <input type="file" name="ExcelFile" onChange={FileImport} />
+
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        defaultColDef={{
+          flex: 1,
+          minWidth: 110,
+          editable: true,
+          resizable: true,
+        }}
+      >
+        <AgGridColumn field="athlete" sortable={true}></AgGridColumn>
+        <AgGridColumn field="age"></AgGridColumn>
+        <AgGridColumn field="country"></AgGridColumn>
+        <AgGridColumn field="year"></AgGridColumn>
+        <AgGridColumn field="date"></AgGridColumn>
+        <AgGridColumn field="sport"></AgGridColumn>
+        <AgGridColumn field="gold"></AgGridColumn>
+        <AgGridColumn field="silver"></AgGridColumn>
+        <AgGridColumn field="bronze"></AgGridColumn>
+        <AgGridColumn field="total"></AgGridColumn>
+      </AgGridReact>
     </div>
   );
 }
