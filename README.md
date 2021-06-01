e4a68d9203dab6cc69b883b9566a7b84334d5a54
Luke-pak <icbm07@hanmail.net>
2021-06-01 15:27:52 +0900

    210601-001

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # SheetJS And React
 
+## 초기 설치
+
+- Install AgGrid `npm install --save ag-grid-community ag-grid-react ag-grid-enterprise`
+
+## Sheet JS
+
 - 엑셀 파일을 읽어 AgGrid에 넣는다.
 - 수정이 가능하고 이후 버튼을 클릭하여 CREATE 되도록
-- `npm install xlsx`
+- Install Sheet JS `npm install xlsx`
+
+## AgGridColumn Component
+
+- AgGridColumn Component [https://www.ag-grid.com/react-grid/component-types/]
+- 위 링크를 잘 참조하여 Cell을 컨트롤 하자. CellRederer, CellEditor등 필요한 부분들이 많다.
--- src/App.js
+++ src/App.js
@@ -1,13 +1,117 @@
 import { useState, useRef } from 'react';
 import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import { Input, Button } from '@material-ui/core';
 import XLSX from 'xlsx';
 
+import 'ag-grid-enterprise';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App() {
-  const [rowData, setRowData] = useState([]);
+  const [gridApi, setGridApi] = useState();
   const gridRef = useRef(null);
+
+  const defaultRowData = [
+    {
+      athlete: 'Gong Jinjie',
+      age: 25,
+      country: 'China',
+      year: 2012,
+      date: 39789,
+      sport: 'Cycling',
+      gold: 0,
+      silver: 1,
+      bronze: 1,
+    },
+    {
+      athlete: 'Olga Kaniskina',
+      age: 27,
+      country: 'Russia',
+      year: 2012,
+      date: 39789,
+      sport: 'Athletics',
+      gold: 0,
+      silver: 1,
+      bronze: 0,
+    },
+    {
+      athlete: 'Vavrinec Hradílek',
+      age: 25,
+      country: 'Czech Republic',
+      year: 2012,
+      date: 39789,
+      sport: 'Canoeing',
+      gold: 0,
+      silver: 1,
+      bronze: 0,
+    },
+    {
+      athlete: 'Jakov Fak',
+      age: 22,
+      country: 'Croatia',
+      year: 2010,
+      date: '28/02/2010',
+      sport: 'Biathlon',
+      gold: 0,
+      silver: 0,
+      bronze: 1,
+    },
+    {
+      athlete: 'Jesse Sergent',
+      age: 24,
+      country: 'New Zealand',
+      year: 2012,
+      date: 39789,
+      sport: 'Cycling',
+      gold: 3,
+      silver: 0,
+      bronze: 1,
+    },
+    {
+      athlete: 'Jeong Seong-Ryong',
+      age: 27,
+      country: 'South Korea',
+      year: 2012,
+      date: 39789,
+      sport: 'Football',
+      gold: 0,
+      silver: 0,
+      bronze: 1,
+    },
+    {
+      athlete: 'Fredrik Lööf',
+      age: 42,
+      country: 'Sweden',
+      year: 2012,
+      date: 39789,
+      sport: 'Sailing',
+      gold: 1,
+      silver: 0,
+      bronze: 0,
+    },
+    {
+      athlete: 'Jo In-Cheol',
+      age: 24,
+      country: 'South Korea',
+      year: 2000,
+      date: 35073,
+      sport: 'Judo',
+      gold: 0,
+      silver: 1,
+      bronze: 0,
+    },
+    {
+      athlete: 'William Lockwood',
+      age: 24,
+      country: 'Australia',
+      year: 2012,
+      date: 39789,
+      sport: 'Rowing',
+      gold: 0,
+      silver: 1,
+      bronze: 0,
+    },
+  ];
 
   const FileImport = e => {
     const reader = new FileReader();
@@ -18,35 +122,71 @@ function App() {
       const wb = XLSX.read(fileData, { type: 'binary' });
       wb.SheetNames.forEach(function (sheetName) {
         var rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
-        setRowData(rowObj);
+        gridApi.setRowData(rowObj);
       });
     };
   };
 
+  const SendJson = () => {
+    let sendData = [];
+    gridApi.forEachNode(node => sendData.push(node.data));
+    console.log(sendData);
+  };
+
   return (
     <div className="ag-theme-alpine" style={{ width: '100vw', height: 800 }}>
-      <input type="file" name="ExcelFile" onChange={FileImport} />
+      <Input type="file" name="ExcelFile" onChange={FileImport} />
+      <Button variant="contained" color="primary" onClick={SendJson}>
+        UPLOAD
+      </Button>
 
       <AgGridReact
         ref={gridRef}
-        rowData={rowData}
+        rowData={defaultRowData}
+        onGridReady={params => setGridApi(params.api)}
+        rowSelection="multiple"
         defaultColDef={{
           flex: 1,
           minWidth: 110,
           editable: true,
           resizable: true,
+          sortable: true,
+          enableRowGroup: true,
+          enablePivot: true,
+          enablePivot: true,
+          floatingFilter: true,
+        }}
+        sideBar={{
+          toolPanels: [
+            {
+              id: 'columns',
+              labelDefault: 'Columns',
+              labelKey: 'columns',
+              iconKey: 'columns',
+              toolPanel: 'agColumnsToolPanel',
+            },
+          ],
         }}
       >
-        <AgGridColumn field="athlete" sortable={true}></AgGridColumn>
-        <AgGridColumn field="age"></AgGridColumn>
-        <AgGridColumn field="country"></AgGridColumn>
-        <AgGridColumn field="year"></AgGridColumn>
+        <AgGridColumn field="athlete" filter="agSetColumnFilter"></AgGridColumn>
+        <AgGridColumn field="age" filter="agNumberColumnFilter"></AgGridColumn>
+        <AgGridColumn field="country" filter="agSetColumnFilter"></AgGridColumn>
+        <AgGridColumn field="year" filter="agSetColumnFilter"></AgGridColumn>
         <AgGridColumn field="date"></AgGridColumn>
         <AgGridColumn field="sport"></AgGridColumn>
         <AgGridColumn field="gold"></AgGridColumn>
         <AgGridColumn field="silver"></AgGridColumn>
         <AgGridColumn field="bronze"></AgGridColumn>
-        <AgGridColumn field="total"></AgGridColumn>
+        <AgGridColumn
+          field="total"
+          // cellRenderer={prop => {
+          //   let total =
+          //     (prop.data.gold || 0) +
+          //     (prop.data.silver || 0) +
+          //     (prop.data.bronze || 0);
+          //   return total;
+          // }}
+        ></AgGridColumn>
       </AgGridReact>
     </div>
   );
